ce25b92655a715a2849e62b66edc16735989033b
Carla Villeneuve <cc191969@ens.uqam.ca>
2023-08-30 10:31:23 -0400

    Ajout autocomplete

--- places.js
+++ places.js
@@ -1,1 +1,30 @@
-console.log("test") 
+let autocomplete;
+
+function initAutoComplete() {
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('adress-input'),
+        {
+            componentRestrictions: {'country' : ['FR'] },
+            fields: [name, geometry]
+        });
+    
+        //autocomplete.addListener('place_changed', onPlaceChanged);
+}
+
+function onPlaceChanged() {
+    var place = autocomplete.getPlace();
+    var distancesContainers = document.getElementsByClassName('distance-to-place-container');
+
+    if (!place.geometry) {
+        document.getElementById('autocomplete').placeholder = 'Tapez votre adresse, code postal, ville';
+        distancesContainers.forEach( (item) => item.style.display = "none");
+    } else {
+        document.getElementById('details').innerHTML = place.name;
+        // Calcul distances dans un array
+
+        for (let i = 0; i < distancesContainers.length; i++){
+            //Calcul distance
+            distancesContainers[i].style.display="flex";
+        }
+    }
+}
